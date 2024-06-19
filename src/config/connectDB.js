4684ff9f62fa5500c78c1a1c2f67563f304c50e9
Invisij <dotuanhung41@gmail.com>
2024-06-19 22:09:00 +0700

    Update: lastest commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,20 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-// // Option 1: Passing a connection URI
-// const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
-// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
-
-// // Option 2: Passing parameters separately (sqlite)
-// const sequelize = new Sequelize({
-//   dialect: 'sqlite',
-//   storage: 'path/to/database.sqlite'
-// });
-
-// Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('homeoffice_dev', 'root', null, {
     host: 'localhost',
-    dialect:
-        'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+    dialect: 'mysql',
     logging: false,
 });
 
